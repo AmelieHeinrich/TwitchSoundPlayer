35bbbe00149c39325623b99c7ba9b79c60d36528
Sausty <MikuoH15TH@gmail.com>
2023-02-25 15:47:41 +0100

    [ADD] Added TSP parser and writer

--- src/tsp_config_parser.cpp
+++ src/tsp_config_parser.cpp
@@ -7,15 +7,60 @@
 
 #include "tsp_config_parser.hpp"
 
+#include <fstream>
+#include <Windows.h>
+#include <string>
+
+
 namespace tsp
 {
+    std::string GetSubstringBefore(const std::string& String, char Character)
+    {
+        std::string::size_type Position = String.find(Character);
+        if (Position != std::string::npos)
+            return String.substr(0, Position);
+        else
+            return String;
+    }
+
+    std::string GetSubstringAfter(const std::string& String, char Character)
+    {
+        std::string::size_type Position = String.find(Character);
+        if (Position != std::string::npos)
+            return String.substr(Position + 1, String.length());
+        else
+            return String;
+    }
+
     Config::Config(const std::string& Path)
     {
-        (void)Path;
+        // Meow=!meow,sounds/meow.wav
+        // Name=command,path
+        std::ifstream Stream(Path);
+        if (!Stream.is_open()) {
+            MessageBoxA(nullptr, "Failed to open config file!", "Error!", MB_ICONERROR);
+        }
+
+        std::string Line;
+        while (std::getline(Stream, Line))
+        {
+            std::string Name = GetSubstringBefore(Line, '=');
+            std::string Command = GetSubstringBefore(GetSubstringAfter(Line, ','), '=');
+            std::string Path = GetSubstringAfter(Line, '=');
+
+            tsp_variable Variable = { Name, Command, Path };
+            mVariables.push_back(Variable);
+        }
+
+        Stream.close();
     }
 
     void Config::Write(const std::string& Path)
     {
-        (void)Path;
+        std::ofstream Stream(Path);
+        for (auto Variable : mVariables) {
+            Stream << Variable.Name << "=" << Variable.Command << "," << Variable.Path << std::endl;
+        }
+        Stream.close();
     }
 }
